ae7a25488205a0eda3fda55fa0bd7bddb1afb52b
ekosenko <evgeni.kosenko@mobisystems.com>
2024-03-24 17:37:59 +0300

    Dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { config } from 'dotenv';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
@@ -14,16 +15,16 @@ import { FavAlbumsEntity } from './favs/entities/fav.albums.entity';
 import { FavArtistsEntity } from './favs/entities/fav.artists.entity';
 import { FavTracksEntity } from './favs/entities/fav.tracks.entity';
 
+config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      // todo put to env
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'eugenekosenko',
-      password: 'root',
-      database: 'rsschool',
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [
         UsersEntity,
         ArtistEntity,
